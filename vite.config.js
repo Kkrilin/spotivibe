2740a335d4772153afb4802cd5100953e940fe4b
kundan singh patel <kundansingh066@gmail.com>
2025-04-08 18:43:15 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 10000, // optional, Render provides $PORT anyway
+    host: true, // binds to 0.0.0.0
+    allowedHosts: ["spotyvibe.onrender.com"],
+  },
+});
